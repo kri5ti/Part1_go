766a222965bd28d0ce30abb344bdf5ade1926330
khalych <khalych>
2017-01-03 19:00:31 +0200

    Add some changes

--- service/main.go
+++ service/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	var s string = r.URL.String()
+	u, err := r.URL.Parse(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	len := len(u.Scheme) + len(u.Host)
+	fmt.Println(len, u.Host, u.Scheme)
+	fmt.Fprintf(w, "Title: %s", u.Path) // send data to client side
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/books/", sayhelloName) // set router
+	err := http.ListenAndServe(":8080", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
